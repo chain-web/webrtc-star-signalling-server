f9e05060d6f14b9a7ec2869ac9acecc9ac68ce37
chengcheng <chengcheng@qcraft.ai>
2022-06-30 19:28:21 +0800

    update sig server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { start } from 'libp2p-webrtc-star-signalling-server';
+import { sigServer } from '@libp2p/webrtc-star-signalling-server';
 
 let server;
 const startServer = async () => {
-  server = await start({
+  server = await sigServer({
     port: 8987,
     host: '0.0.0.0',
-    metrics: false,
+    metrics: true,
   });
 };
 
